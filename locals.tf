d48a3f7669162fa63e70c9fff78e092bf812d87e
kavya Poreddy <poreddykavya1207@gmail.com>
2026-05-30 15:48:56 +0530

    fix private subnet ids reference

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   common_name_suffix = "${var.project_name}-${var.environment}"
   private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_id.value)[0]
-  private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_id.value)
+  private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   sg_id = data.aws_ssm_parameter.sg_id.value
   tg_port = "${var.component}" == "frontend" ? 80 : 8080
