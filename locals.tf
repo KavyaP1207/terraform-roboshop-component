90060dbf2598f6b43e9e0850a160e984f79ff0e4
kavya Poreddy <poreddykavya1207@gmail.com>
2026-05-30 15:56:31 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   common_name_suffix = "${var.project_name}-${var.environment}"
-  private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_id.value)[0]
+  private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
   private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   sg_id = data.aws_ssm_parameter.sg_id.value
